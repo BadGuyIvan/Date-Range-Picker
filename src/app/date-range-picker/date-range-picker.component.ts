5dc18f8e1f14fa6bc2cee080fb2fcb67d9836ca5
BadGuyIvan <badguy.ivan.i@gmail.com>
2019-12-30 17:20:26 +0200

    range_selection

--- src/app/date-range-picker/date-range-picker.component.ts
+++ src/app/date-range-picker/date-range-picker.component.ts
@@ -1,38 +1,61 @@
-import { Component, OnInit, AfterViewInit, ViewChildren, QueryList, ElementRef, Renderer2, OnDestroy } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  AfterViewInit,
+  ViewChildren,
+  QueryList,
+  ElementRef,
+  Renderer2,
+  OnDestroy
+} from "@angular/core";
 import * as moment from "moment";
-import { chunk } from '../shared/chunk';
-import { take, scan, repeat } from 'rxjs/operators';
-import { fromEvent, merge, Subscription } from 'rxjs';
+import { chunk } from "../shared/chunk";
+import {
+  take,
+  scan,
+  repeat,
+  switchMap,
+  startWith,
+  takeWhile
+} from "rxjs/operators";
+import { fromEvent, merge, Subscription } from "rxjs";
 
 @Component({
-  selector: 'app-date-range-picker',
-  templateUrl: './date-range-picker.component.html',
-  styleUrls: ['./date-range-picker.component.scss'],
+  selector: "app-date-range-picker",
+  templateUrl: "./date-range-picker.component.html",
+  styleUrls: ["./date-range-picker.component.scss"]
 })
-export class DateRangePickerComponent implements OnInit, AfterViewInit, OnDestroy {
+export class DateRangePickerComponent
+  implements OnInit, AfterViewInit, OnDestroy {
+  constructor(private element: ElementRef, private renderer: Renderer2) {}
 
-  constructor(private element: ElementRef, private renderer: Renderer2) { }
-
-  @ViewChildren('weekday') days_of_the_month: QueryList<ElementRef>;
+  @ViewChildren("weekday") days_of_the_month: QueryList<ElementRef>;
 
   dayClickSubscription: Subscription;
   viewChangesSubscription: Subscription;
 
   date = moment();
-  currentMonthDates: any[]
-  weekDays = moment.weekdays()
+  currentMonthDates: any[];
+  weekDays = moment.weekdays();
 
   previousMonth() {
-    this.date = moment(this.date.subtract(1, 'months'))
-    this.createDatePicker()
+    this.date = moment(this.date.subtract(1, "months"));
+    this.createDatePicker();
   }
   nextMonth() {
-    this.date = moment(this.date.add(1, 'months'))
-    this.createDatePicker()
+    this.date = moment(this.date.add(1, "months"));
+    this.createDatePicker();
   }
 
   createDatePicker() {
-    this.currentMonthDates = new Array(moment(this.date).daysInMonth()).fill(null).map((x, i) => moment(this.date).startOf('month').add(i, 'days').toDate());
+    this.currentMonthDates = new Array(moment(this.date).daysInMonth())
+      .fill(null)
+      .map((x, i) =>
+        moment(this.date)
+          .startOf("month")
+          .add(i, "days")
+          .toDate()
+      );
 
     // var range_42 = new Array(42).fill(null).map(
     //   (_, i) => {
@@ -45,21 +68,26 @@ export class DateRangePickerComponent implements OnInit, AfterViewInit, OnDestro
     //   });
 
     if (this.currentMonthDates[0].getDay() > 0) {
-
       //calculate days before the first day in the month.
       let days = new Array(this.currentMonthDates[0].getDay()).fill(null);
 
       //If the month doesn't start from Sunday, then it is necessary to add days to the start array.
       this.currentMonthDates.unshift(...days);
-
     }
 
-    // if (this.currentMonthDates[this.currentMonthDates.length - 1].getDay() < 6) {
-    //   //calculate days after the last day in the month.
-    //   let days = new Array(6 - this.currentMonthDates[this.currentMonthDates.length - 1].getDay()).fill(null);
+    if (
+      this.currentMonthDates[this.currentMonthDates.length - 1].getDay() < 6
+    ) {
+      //calculate days after the last day in the month.
+      let days = new Array(
+        6 -
+          moment(this.date)
+            .endOf("month")
+            .weekday()
+      ).fill(null);
 
-    //   this.currentMonthDates.push(...days);
-    // }
+      this.currentMonthDates.push(...days);
+    }
 
     // this.currentMonthDates = chunck(range_42, 7);
 
@@ -67,60 +95,100 @@ export class DateRangePickerComponent implements OnInit, AfterViewInit, OnDestro
   }
 
   onSelectedDate() {
-    var Clicks = this.days_of_the_month.filter(((item: ElementRef) => {
-      return item.nativeElement.className !== 'offset';
-    })).map((t) => fromEvent(t.nativeElement, 'click'))
+    let clickEvent = this.days_of_the_month
+      .filter((item: ElementRef) => {
+        return item.nativeElement.className !== "offset";
+      })
+      .map(t => fromEvent(t.nativeElement, "click"));
 
-    this.dayClickSubscription = merge(...Clicks).pipe(
-      take(2),
-      scan((arr: Array<EventTarget>, prev: MouseEvent) => arr.concat(prev.target), []),
-      repeat(),
-    ).subscribe(
-      (event: Array<EventTarget>) => {
-        let [startDate, endDate] = event.map((element: Element) => Date.parse(element.attributes['day'].value))
+    let mouseEnter = this.days_of_the_month
+      .filter((item: ElementRef) => {
+        return item.nativeElement.className !== "offset";
+      })
+      .map(t => fromEvent(t.nativeElement, "mouseenter"));
 
-        //if a user chooses first day bigger than end day need to swap these two days
-        if (startDate > endDate) {
-          event = [event[1], event[0]]
-        }
+    this.dayClickSubscription = merge(...clickEvent)
+      .pipe(
+        take(2),
+        scan(
+          (item, val) => ({
+            count: item.count + 1,
+            targer: val
+          }),
+          {
+            count: 0,
+            targer: null
+          }
+        ),
+        switchMap(event =>
+          merge(...mouseEnter).pipe(
+            //after the first click, we have to start from this event, that's why we need to use 'startWith'
+            startWith(event.targer),
+            takeWhile(() => event.count !== 2)
+          )
+        ),
+        scan((arr: Array<EventTarget>, prev: MouseEvent) => {
+          if (arr.length !== 2) {
+            arr = arr.concat(prev.target);
+          } else {
+            arr[1] = prev.target;
+          }
+          return arr;
+        }, []),
+        repeat()
+      )
+      .subscribe(
+        event => {
+          let [startDate, endDate] = event.map(
+            (element: Element) => new Date(element.attributes["day"].value)
+          );
 
-        let [index_of_the_first_day, index_of_the_last_day] = event.map((searchItem) => {
-          let index = this.days_of_the_month.toArray().findIndex(item => item.nativeElement === searchItem);
-          return index
-        })
+          //if a user chooses first day bigger than end day need to swap these two days
+          if (startDate && endDate && startDate.getTime() > endDate.getTime()) {
+            event = [event[1], event[0]];
+          }
 
-        if (endDate) {
-          //adding a class to the range of selected days
-          this.days_of_the_month.toArray().slice(index_of_the_first_day, index_of_the_last_day + 1).forEach(item => {
-            this.renderer.addClass(item.nativeElement, 'selected')
-          });
-        } else {
-          //If the range of days has already been selected, you must remove the "selected" class
-          this.days_of_the_month.toArray().forEach((item) => {
-            if (/selected/.test(item.nativeElement.classList.value)) {
-              this.renderer.removeClass(item.nativeElement, 'selected')
+          let [index_of_the_first_day, index_of_the_last_day] = event.map(
+            searchItem => {
+              let index = this.days_of_the_month
+                .toArray()
+                .findIndex(item => item.nativeElement === searchItem);
+              return index;
             }
-          })
-          this.renderer.addClass(event[0], 'selected');
-        }
-      },
-    )
+          );
+
+          this.days_of_the_month.toArray().forEach(item => {
+            if (/selected/.test(item.nativeElement.classList.value)) {
+              this.renderer.removeClass(item.nativeElement, "selected");
+            }
+          });
+          //adding a class to the range of selected days
+          this.days_of_the_month
+            .toArray()
+            .slice(index_of_the_first_day, index_of_the_last_day + 1)
+            .forEach(item => {
+              this.renderer.addClass(item.nativeElement, "selected");
+            });
+        },
+        err => console.error(err)
+      );
   }
 
   ngOnInit() {
-    this.createDatePicker()
+    this.createDatePicker();
   }
 
   ngAfterViewInit(): void {
-    this.viewChangesSubscription = this.days_of_the_month.changes.subscribe(() => {
-      this.onSelectedDate();
-    })
-    this.onSelectedDate()
+    this.viewChangesSubscription = this.days_of_the_month.changes.subscribe(
+      () => {
+        this.onSelectedDate();
+      }
+    );
+    this.onSelectedDate();
   }
 
   ngOnDestroy(): void {
     this.dayClickSubscription.unsubscribe();
     this.viewChangesSubscription.unsubscribe();
   }
-
 }
